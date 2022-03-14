99d109dfb507de746b7bb43378f33951a6b54be0
José Antonio Berrón Gutiérrez <61696362+JoseBG-spec@users.noreply.github.com>
2022-03-14 14:33:25 -0600

    changes

--- GeneradorBasicoANTLR/listeners/gencode.py
+++ GeneradorBasicoANTLR/listeners/gencode.py
@@ -4,61 +4,61 @@ from marzo.marzoParser import marzoParser
 class GenCode(marzoListener):
 
     def __init__(self):
-        self.var = 0
-        self.queue = []
-        self.stack = []
+        self.x = 0
+        self.list = []
+        self.list2 = []
     
     def enterProgram(self, ctx:marzoParser.ProgramContext):
         print(".text")
 
     def exitNumero(self, ctx: marzoParser.NumeroContext):
-        self.var+=1
-        self.queue.append(self.var)
-        self.stack.append(self.var)
-        print("li $v" + str(self.var) + ", " + ctx.Numero().getText())
+        self.x+=1
+        self.list.append(self.x)
+        self.list2.append(self.x)
+        print("li $v" + str(self.x) + ", " + ctx.Numero().getText())
     
     def exitLetra(self, ctx: marzoParser.LetraContext):
-        self.var+=1
-        self.queue.append(self.var)
-        self.stack.append(self.var)
-        print("lw $v" + str(self.var) + ", " + ctx.Letra().getText())
+        self.x+=1
+        self.list.append(self.x)
+        self.list2.append(self.x)
+        print("lw $v" + str(self.x) + ", " + ctx.Letra().getText())
 
     def exitSuma(self, ctx:marzoParser.SumaContext):
-        print("ADD $v" + str((self.var)) +", " + "$v" + str(self.stack.pop()) + ", " + "$v" + str(self.stack.pop()))
+        print("ADD $v" + str((self.x)) +", " + "$v" + str(self.list2.pop()) + ", " + "$v" + str(self.list2.pop()))
 
     def exitResta(self, ctx: marzoParser.RestaContext):
-        print("REST $v" + str((self.var)) +", " + "$v" + str(self.stack.pop()) + ", " + "$v" + str(self.stack.pop()))
+        print("REST $v" + str((self.x)) +", " + "$v" + str(self.list2.pop()) + ", " + "$v" + str(self.list2.pop()))
 
     def exitMultiplicacion(self, ctx: marzoParser.MultiplicacionContext):
-        print("MULT $v" + str((self.var)) +", " + "$v" + str(self.stack.pop()) + ", " + "$v" + str(self.stack.pop()))
+        print("MULT $v" + str((self.x)) +", " + "$v" + str(self.list2.pop()) + ", " + "$v" + str(self.list2.pop()))
 
     def exitDivision(self, ctx: marzoParser.DivisionContext):
-        print("DIV $v" + str((self.var)) +", " + "$v" + str(self.stack.pop()) + ", " + "$v" + str(self.stack.pop()))
+        print("DIV $v" + str((self.x)) +", " + "$v" + str(self.list2.pop()) + ", " + "$v" + str(self.list2.pop()))
     
     def exitAsignacion(self, ctx:marzoParser.AsignacionContext):
-        print("li $v" + str((self.var)) +", " + "$v" + str(self.stack.pop()) + ", " + "$v" + str(self.stack.pop()))
+        print("li $v" + str((self.x)) +", " + "$v" + str(self.list2.pop()) + ", " + "$v" + str(self.list2.pop()))
 
     def exitDeclaracion(self, ctx:marzoParser.DeclaracionContext):
-        self.var+=1
-        self.queue.append(self.var)
-        self.stack.append(self.var)
-        print("sw $v" + str(self.var) + ", " + ctx.Letra().getText())
+        self.x+=1
+        self.list.append(self.x)
+        self.list2.append(self.x)
+        print("sw $v" + str(self.x) + ", " + ctx.Letra().getText())
 
     def exitMayor(self, ctx:marzoParser.MayorContext):
-        print("stgreater $v" + str((self.var)) +", " + "$v" + str(self.stack.pop(1)) + ", " + "$v" + str(self.stack.pop()))
+        print("stgreater $v" + str((self.x)) +", " + "$v" + str(self.list2.pop(1)) + ", " + "$v" + str(self.list2.pop()))
 
     def exitMenor(self, ctx:marzoParser.MenorContext):
-        print("sllower $v" + str((self.var)) +", " + "$v" + str(self.stack.pop(1)) + ", " + "$v" + str(self.stack.pop()))
+        print("sllower $v" + str((self.x)) +", " + "$v" + str(self.list2.pop(1)) + ", " + "$v" + str(self.list2.pop()))
     
     def exitPrint(self, ctx:marzoParser.PrintContext):
         print("syscall")
         print("______")
 
     def exitIf(self, ctx:marzoParser.IfContext):
-        print("bequals $v" + str(self.queue.pop(0)) +", $v" + str(self.queue.pop(0)) + ", ELSE")
+        print("bequals $v" + str(self.list.pop(0)) +", $v" + str(self.list.pop(0)) + ", ELSE")
 
     def exitIfnoelse(self, ctx: marzoParser.IfnoelseContext):
-        print("bequals $v" + str(self.queue.pop(0)) +", $v" + str(self.queue.pop(0)))
+        print("bequals $v" + str(self.list.pop(0)) +", $v" + str(self.list.pop(0)))
     
     def exitEndStatement(self, ctx: marzoParser.EndStatementContext):
         print("-----------------------------------------")
